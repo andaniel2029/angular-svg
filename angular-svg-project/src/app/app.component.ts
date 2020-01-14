60f14f675b96b7757f559a88bf36c26860d0344a
Rcdev <rcdev198739@gmail.com>
2020-01-13 21:04:32 -0500

    Update data fetch

--- angular-svg-project/src/app/app.component.ts
+++ angular-svg-project/src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-svg-project';
+  portCount: number;
+  fanCount: number;
+  powerCount: number;
+
+  isDataFetched: boolean = false;
+
+  portColors: Array<string> = [];
+
+
+  getData(data) {
+    if (data) {
+      this.portCount = data.numberOfPorts;
+      this.fanCount = data.numberOfFans;
+      this.powerCount = data.numberOfPower;
+
+      this.isDataFetched = true;
+      console.log('******', this.generateRandomColor());
+    }
+  }
+
+  generateRandomColor() {
+    return Math.floor(Math.random()*16777215).toString(16);
+  }
 }
